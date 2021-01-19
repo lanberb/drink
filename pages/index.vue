4893de46da79139fbfc2e488b72dd7c8856ed5f2
Nao Sasaki <lanberb.2@gmail.com>
2021-01-19 12:29:36 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div>
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function(){ 
+    return{
+      message: ''
+    }
+  },
+  asyncData: async function(){
+    return axios.post('https://id.smaregi.dev/userinfo/').then(response => {
+      console.log(response.data);
+      return {
+        message: response.data
+      }
+    }).catch(console.error);
+  },
+}
+</script>
+
+<style> 
+</style>
